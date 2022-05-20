a1302515399b2577158f3c444ee1e2606b7cd41c
Javier Villegas <javillegasna@gmail.com>
2022-05-20 02:32:24 +0000

    fix lose render

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,16 +1,16 @@
 import "./App.css";
 import Layout from "../layout";
 import Home from "../views/home/Home";
-import { BrowserRouter, Routes ,Route } from "react-router-dom";
+import { HashRouter, Routes ,Route } from "react-router-dom";
 import PAnimation from "../views/3DAnimation/PAnimation";
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
     <Routes>
        <Route path="/" element ={ <Layout><Home/></Layout>} />
        <Route path="/project_1" element={<Layout><PAnimation/></Layout>}/>
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
